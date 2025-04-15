2413ddbd48c251429cf86305051c4c4b89324700
Yushabh Dhande <yush2428@gmail.com>
2025-04-15 13:51:17 +0530

    feat: header component created

--- resources/js/pages/landing.tsx
+++ resources/js/pages/landing.tsx
@@ -1,9 +1,13 @@
+import Header from '@/components/sections/header';
 import { Head } from '@inertiajs/react';
 
 export default function Landing() {
     return (
         <>
             <Head title="Portfolio" />
+            <div className="app">
+                <Header />
+            </div>
         </>
     );
 }
