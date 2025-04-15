2f5a9983c5ecd150ee60b0d3f35bbd43de77aa9f
Yushabh Dhande <yush2428@gmail.com>
2025-04-15 13:37:00 +0530

    feat: landing page component added and route created for the same

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/home', function () {
+    return Inertia::render('landing');
+})->name('home');
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
