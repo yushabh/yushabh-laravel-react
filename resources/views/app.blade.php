deffff4f4d9174f336ac9e55fe8935d26907e80e
Yushabh Dhande <yush2428@gmail.com>
2025-04-17 18:37:39 +0530

    seo: sitemap and robot txt files added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Web Developer in Gujarat, India. I specialize in building exceptional digital experiences. Specializing in React and Laravel, I create user-friendly and efficient web applications.">
+        <meta name="keywords" content="Laravel Developer, Web developer, PHP developer, PHP framework, Portfolio, Web Development, React, Laravel, Freelance Developers, Freelancer, 'SENIOR PHP DEVELOPER'">
+        <meta name="author" content="Yushabh Dhande">
+        <title inertia>{{ config('app.name', 'Yushabh Dhande') }}</title>
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
@@ -29,8 +33,6 @@
                 background-color: oklch(0.145 0 0);
             }
         </style>
-
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
